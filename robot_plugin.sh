01c9a3a369f754efded09210cb2181c57e50de08
xing-laptop <lx704612715@gmail.com>
2024-07-20 12:39:14 +0200

    latex and ros

---
+++ robot_plugin.sh
@@ -0,0 +1,2 @@
+### Install necessary packages 
+sudo apt-get install -y python3-pip python3-dev xclip xsel python3-numba python3-pygraphviz python3-pyqtgraph python3-pyqt5 terminator python3-catkin-pkg python3-rosdep2
--- ros_install.sh
+++ ros_install.sh
@@ -62,7 +62,7 @@ echo "Download the ROS keys"
 roskey=`apt-key list | grep "ROS Builder"` && true # make sure it returns true
 if [ -z "$roskey" ]; then
   echo "No ROS key, adding"
-  sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 421C365BD9FF1F717815A3895523BAEEB01FA116
 fi
 
 echo "Updating & upgrading all packages"
